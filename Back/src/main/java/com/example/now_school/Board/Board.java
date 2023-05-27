931badae98b3a7fd5ca0420d9682a9f182b3783f
min-0 <dhe08133@naver.com>
2023-05-28 00:00:44 +0900

    chore: mySQL 연동

--- Back/src/main/java/com/example/now_school/Board/Board.java
+++ Back/src/main/java/com/example/now_school/Board/Board.java
@@ -1,20 +1,41 @@
 package com.example.now_school.Board;
 
 import lombok.*;
+import org.hibernate.annotations.Columns;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "board")
 @AllArgsConstructor
 @Builder
+@Entity(name = "board")
 public class Board {
-    private Long id;
-    private String Title;
+    @Id
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
     private String content;
+
+    @Column(name = "writeTime")
     private LocalDateTime writeTime;
+
+    @Column(name = "userId")
     private String userId;
+
+    @Column(name = "userPw")
     private String userPw;
+
+    @Column(name = "image")
     private String image;
 }
--- Back/src/main/java/com/example/now_school/Board/BoardController.java
+++ Back/src/main/java/com/example/now_school/Board/BoardController.java
@@ -5,15 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/board")
 @RequiredArgsConstructor
 public class BoardController {
 
-    private static BoardRepository boardRepository;
+    private final BoardRepository boardRepository;
 
-    @GetMapping("/test")
-    public String Hello() {
-        return "Hello";
+    @GetMapping
+    public Optional<Board> read(String id){
+        return  boardRepository.findByUserId(id);
     }
 }
